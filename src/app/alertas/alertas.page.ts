1e9f695f4a9fda9777eba23b8c0fbc7646ea7cc5
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-01-14 16:22:45 -0500

    alerts finish

--- src/app/alertas/alertas.page.ts
+++ src/app/alertas/alertas.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-alertas',
@@ -7,9 +8,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertasPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController:AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlertMultipleButtons(){
+    const alert = await this.alertController.create({
+      cssClass:'my-custom-class',
+      header:'Alert',
+      subHeader:'Subtitle',
+      message:'this is alert: multiple buttons',
+      buttons:[{
+        text:'OK',
+        handler:()=>{
+          console.log('OK enviado ')
+        }
+      },
+        {
+          text:"Cancel",
+          role:'cancel',
+          cssClass:'danger',
+          handler:()=>{console.log("cancelado")}
+        }]
+    })
+    await alert.present()
+  }
+
+  async loadForm(){
+    const form=await this.alertController.create({
+      cssClass:'my-custom-class',
+      header:'Form',
+      subHeader:'Load your data',
+      message:'Fill all blank spaces',
+      inputs:[
+        {
+          name:'txtName',
+          type:'text',
+          placeholder:'Here your name'
+        },
+        {
+          name:'txtID',
+          type:"number",
+          placeholder:"Your identification card number"
+        },
+        {
+          name:'txtEmail',
+          type:"email",
+          placeholder:'Your email'
+        }
+
+      ],
+      buttons:[
+        {
+          text:'Send',
+          handler:()=>{console.log('send successfull')}
+        },
+        {
+          text:'Cancel',
+          role:'cancel',
+          cssClass:"secondary",
+          handler:()=>{console.log('operation canceled')}
+        }
+      ]
+    })
+    await form.present()
   }
 
 }
