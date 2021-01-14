2ed9cb2feecb5225945dc10486b6204cad6992c6
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-01-14 17:51:31 -0500

    slides in card

--- src/app/tarjetas/tarjetas.page.ts
+++ src/app/tarjetas/tarjetas.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertController} from '@ionic/angular';
+import {title} from 'process';
 
 @Component({
   selector: 'app-tarjetas',
@@ -7,9 +9,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TarjetasPage implements OnInit {
 
-  constructor() { }
+  newObjets:dataImg[]=[
+  {
+    img:'/assets/img/shanks.jpg',
+    alt:'Akagami no Shanks',
+    label:"Akagami no Shanks"
+  },
+  {
+    img:'/assets/img/law.jpg',
+    alt:'Trafalgar D Walter Law',
+    label:"Trafalgar D Walter Law"
+  },
+  {
+    img:'/assets/img/luffy.jpg',
+    alt:'Monkey D Luffy',
+    label:"Monkey D Luffy"
+  },
+  ]
+
+  cards:card[]=[
+    {
+      img:'/assets/img/zoro.jpg',
+      alt:'Roronoa Zoro',
+      title:'Roronoa Zoro',
+      subtitle:"Zoro",
+      content:'Espadachin de la tripulacion del sombrero de paja'
+    },
+    {
+      img:'/assets/img/luffy.jpg',
+      alt:'Roronoa Zoro',
+      title:'Roronoa Zoro',
+      subtitle:"Zoro",
+      content:'Espadachin de la tripulacion del sombrero de paja'
+    },
+    {
+      img:'/assets/img/shanks.jpg',
+      alt:'Roronoa Zoro',
+      title:'Roronoa Zoro',
+      subtitle:"Zoro",
+      content:'Espadachin de la tripulacion del sombrero de paja'
+    },
+  ]
+
+  constructor(private alert:AlertController) { }
 
   ngOnInit() {
   }
 
+  async person(){
+    const flagAlert = await this.alert.create({
+      cssClass:'my-custom-class',
+      header:`Person`,
+      message:'hola',
+      buttons:['OK']
+    })
+    await flagAlert.present()
+  }
+
 }
+ 
+interface dataImg{
+  img:string;
+  alt:string;
+  label:string;
+}
+
+interface card{
+  img:string;
+  alt:string;
+  title:string;
+  subtitle:string;
+  content:string;
+} 
