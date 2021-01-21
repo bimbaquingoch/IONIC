d5805b18043c5eb57a3b3fafe37c4328c9a36c66
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-01-21 00:45:55 -0500

    deber terminado cosumiendo desde API rickandmortyapi todo funciona

--- src/app/deber/deber.page.ts
+++ src/app/deber/deber.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {RickandmortyService} from '../services/rickandmorty.service';
 // const fetchData = require ('./utils/fetchData')
 
-const API = "https://rickandmortyapi.com/api/character/";
+// const API = "https://rickandmortyapi.com/api/character/";
 
 @Component({
   selector: 'app-deber',
@@ -11,7 +11,7 @@ const API = "https://rickandmortyapi.com/api/character/";
 })
 export class DeberPage implements OnInit {
 
-  caracter=[{},{}]
+  names :any [] = [];
 
   constructor(private rickandmortyservice:RickandmortyService) { }
 
@@ -20,19 +20,10 @@ export class DeberPage implements OnInit {
 
   ionViewDidEnter(){
     this.rickandmortyservice.getInfo().then((newinfo)=>{
-      for (let i = 0; i <20 ; i++) {
-
-        this.caracter = newinfo.results[i].name;
-        var origin = newinfo.results[i].origin.name;
-        var gender = newinfo.results[i].gender;
-        var specie = newinfo.results[i].species;
-        console.log(this.caracter);
-        console.log(origin)
-        console.log(gender)
-        console.log(specie)
-
-      }
+      this.names = newinfo.results;
+     console.log(this.names);
     })
   }
 }
+
 
