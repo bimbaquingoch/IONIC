13d5ce10c6b1a9b1f36941c47d7c27e7b3c4ee8b
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-01-21 01:33:12 -0500

    coment this.name deber.page.ts

--- src/app/deber/deber.page.ts
+++ src/app/deber/deber.page.ts
@@ -21,7 +21,7 @@ export class DeberPage implements OnInit {
   ionViewDidEnter(){
     this.rickandmortyservice.getInfo().then((newinfo)=>{
       this.names = newinfo.results;
-     console.log(this.names);
+     // console.log(this.names);
     })
   }
 }
