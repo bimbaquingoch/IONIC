048699930f41a31f9fd80e00db8653ec11524fd7
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-02-12 17:36:55 -0500

    cambios nuevos

--- src/app/services/rickandmorty.service.ts
+++ src/app/services/rickandmorty.service.ts
@@ -1,13 +1,12 @@
-import { Injectable } from '@angular/core';
-const API = "https://rickandmortyapi.com/api/character/?page=5";
+import { Injectable } from "@angular/core";
+const API = "https://rickandmortyapi.com/api/character/?page=1";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class RickandmortyService {
-
-  constructor() { }
-  getInfo(){
-    return fetch(API).then( response => response.json());
+  constructor() {}
+  getInfo() {
+    return fetch(API).then((response) => response.json());
   }
 }
